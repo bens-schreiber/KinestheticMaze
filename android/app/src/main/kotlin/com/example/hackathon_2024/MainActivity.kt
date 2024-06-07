b4aec15c571f71e1e69ce52842117676fe0c7bd5
Benjamin Schreiber <Ben.Schreiber@IntelliTect.com>
2024-06-07 15:01:11 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/hackathon_2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hackathon_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(color: Colors.blue));
+  }
+}
